c083b595adc2fc84f44842aa47f05c8db41bab6a
MykolaLucky <151729673+MykolaLucky@users.noreply.github.com>
2023-11-29 17:19:38 +0200

    1
    1

---
+++ lab2/lab_2_Mykola Ryzun.py
@@ -0,0 +1,12 @@
+#1
+print("Programming", "Essential", "in...",sep="***",end="")
+print("Python",)
+#2
+print("      *","\n     * *","\n    *   * ","\n   *     *","\n  ***   *** ","\n    *   *","\n    *   *","\n    *****")
+#3
+print("I\'m student")
+#4
+print("\"I\'m\"","\n\"\"learning\"\"","\n\"\"\"Python\"\"\"")
+#5
+print(0o500)
+print(0x777)
